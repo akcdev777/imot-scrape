ed4c0f1f22e7b8aa866d308f89c7c1641c092749
akcdev777 <76046364+akcdev777@users.noreply.github.com>
2024-07-12 09:47:07 +0100

    capture currency in a separate value

--- mongoconnect.py
+++ mongoconnect.py
@@ -1,4 +1,18 @@
+import pandas as pd
 from pymongo import MongoClient
 
+# MongoDB setup
+print("Connecting to MongoDB...")
+mongo_client = MongoClient('mongodb://localhost:27017/')
+db = mongo_client['imot-scrape']
+collection = db['imoti-for-sale']
 
-mongo_client = MongoClient('mongodb://localhost:27017/imot-scrape')
+# Import CSV data into MongoDB
+print("Importing CSV...")
+df = pd.read_csv('properties.csv')
+
+# Convert DataFrame to dictionary and insert into MongoDB
+print("Inserting data into MongoDB...")
+data_dict = df.to_dict("records")
+collection.insert_many(data_dict)
+print(f"Inserted {len(data_dict)} records from CSV into MongoDB.")
